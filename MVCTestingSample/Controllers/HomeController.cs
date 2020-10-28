bde2979abff5071bc3c0eb95c8724064235bb5ea
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-28 15:25:27 -0700

    Created test to validate HomeController.Index

--- MVCTestingSample/Controllers/HomeController.cs
+++ MVCTestingSample/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace MVCTestingSample.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public HomeController()
+        {
+
+        }
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
