52576e26e7dcb2262f24fc4bfeb4582d1e73e0e9
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-28 16:29:11 -0700

    Added Product model, Created DbContext, Created ProductRepository and default implementation

---
+++ MVCTestingSample/Models/ProductDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCTestingSample.Models
+{
+    public class ProductDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<Product> Products { get; set; }
+    }
+}
